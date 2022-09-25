2f5eb95d0363ff71f9ffd281fc7d477da73bc04d
EC2 Default User <ec2-user@ip-172-31-10-125.ec2.internal>
2022-09-25 06:29:12 +0000

    devise導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "642ee502e9e24e549f4bd5fa7ef407c4.vfs.cloud9.us-east-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  # 顧客用
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  # 管理者用
+  devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  root to: "homes#top"
+  get '/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
